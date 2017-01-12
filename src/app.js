ef331cac82de861c82d65628c1cf2b2706c8ae3d
Chris Bendel <chrissbendel@gmail.com>
2017-01-12 16:45:41 -0500

    remove unused files

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 GLOBAL = require('../src/Globals');
 
-import React, { Component } from 'react';
-// import React from 'react-native';
-import { Text, Platform, Navigator, TouchableHighlight } from 'react-native';
+import React, {Component} from 'react';
+import {Text, Platform, Navigator, TouchableHighlight} from 'react-native';
 
 import Home from './Home';
 import Room from './Room';
-import api from './api';
+import DubAPI from './DubAPI/';
 
 export default class app extends Component {
   constructor(props) {
     super(props);
-    this.api = new api();
-
-    this.api.startApi({username: 'dubtrackmobile', password: 'insecure'}, function(err, api) {
-      if (err) console.error(err);
-
+    var api = new DubAPI({username: 'dubtrackmobile', password: 'insecure'}, function (err, user) {
+      console.log(user);
       console.log(api);
-
+      console.log(err);
     });
-
-    this.api.joinRoom('daftlabs');
   }
 
   render() {
@@ -31,12 +25,13 @@ export default class app extends Component {
       />
     );
   }
+
   renderScene(route, navigator) {
-    //TODO: make this a switch statement for all routes
-    if(route.name == 'Home') {
+    //TODO: make this a switch statement
+    if (route.name == 'Home') {
       return (
         <Home
-          navigator = {navigator}
+          navigator={navigator}
           {...route.passProps}
         />
       );
@@ -44,7 +39,7 @@ export default class app extends Component {
     if (route.name == 'Room') {
       return (
         <Room
-          navigator = {navigator}
+          navigator={navigator}
           {...route.passProps}
         />
       );
