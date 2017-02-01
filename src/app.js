af036576b626f1822f2147cc0c0ec2ba648800fc
Chris Bendel <chrissbendel@gmail.com>
2017-02-01 17:50:19 -0500

    added nativebase to code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 GLOBAL = require('../src/Globals');
 
 import React, {Component} from 'react';
-import {View, StyleSheet, Navigator} from 'react-native';
+import {View, StyleSheet, Navigator, Image} from 'react-native';
 
 import Home from './Home';
 import Room from './Room';
@@ -10,6 +10,7 @@ import Settings from './Settings';
 import {Actions, Scene, Router} from 'react-native-router-flux';
 //TODO: use store to save user's login credentials/session
 import store from 'react-native-simple-store';
+import Bars from './icons/bars.png'
 
 export default class app extends Component {
   static user = new Client();
@@ -22,8 +23,12 @@ export default class app extends Component {
   render() {
     return (
       <Router sceneStyle={{backgroundColor:'#F7F7F7'}}>
-        <Scene key="root">
-          <Scene key="home" initial={true} component={Home} hideNavBar={true} title="Home"/>
+        <Scene key="root" hideNavBar={true}>
+          <Scene key="home"
+                 initial={true}
+                 component={Home}
+                 title="Home"
+          />
           <Scene key="room" component={Room} user={app.user} title="Room"/>
           <Scene key="settings" component={Settings} title="Settings">
             {/*<Scene key="login" component={Login}/>*/}
