6a01648080ece4b5a96c0a13726d5c9ea216a214
Chris Bendel <chrissbendel@gmail.com>
2017-02-19 20:14:22 -0500

    this refactor is sketch af

--- src/Home.js
+++ src/Home.js
@@ -25,8 +25,23 @@ import {
 } from 'react-native-card-view';
 
 import app from './app';
+import Settings from './Settings';
 import {Actions} from 'react-native-router-flux'
-import {Container, Header, Title, Left, Right, Body, Button, Icon, Content} from 'native-base';
+import {
+  Container,
+  Header,
+  Title,
+  Left,
+  Item,
+  Right,
+  Footer,
+  Input,
+  FooterTab,
+  Body,
+  Button,
+  Icon,
+  Content
+} from 'native-base';
 import Spinner from 'react-native-loading-spinner-overlay';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 let {height, width} = Dimensions.get('window');
@@ -34,34 +49,33 @@ let {height, width} = Dimensions.get('window');
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    console.log(app.user);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       roomSearch: '',
       dataSource: ds.cloneWithRows([]),
       refreshing: false,
       loading: false,
+      settingsHidden: false,
     };
     app.user.login('dubtrackmobile', 'insecure');
     app.user.setSocket()
       .then(() => {
-        app.user.socket.on('message', function (msg) {
-          msg = JSON.parse(msg);
-          console.log(msg);
-        })
+        this.setPMListener();
       });
   }
 
   componentDidMount() {
     this.loadData();
-    // app.user.socket.on('message', function (msg) {
-    //   msg = JSON.parse(msg);
-    //   console.log(msg);
-    // })
   }
 
   componentWillMount() {
+  }
 
+  setPMListener() {
+    app.user.socket.on('message', function (msg) {
+      msg = JSON.parse(msg);
+      console.log(msg);
+    })
   }
 
   loadData(room) {
@@ -101,25 +115,24 @@ export default class Home extends Component {
 
   //use command+shift+k to enable keyboard hardware on ios emulator to test search bar
   render() {
-    let that = this;
     return (
-      <View style={{flex: 1}}>
         <Container>
-          <Header>
-            <Left>
-              <Button transparent onPress={() => Actions.refresh({key: 'menu', open: value => !value})}>
-                <Icon size={30} name={'ios-menu'}>
-                </Icon>
-              </Button>
-            </Left>
-            <Body>
-              <Title>Lobby</Title>
-            </Body>
-            <Right>
-              <Button transparent onPress={() => Actions.refresh({key: 'messages', open: value => !value})}>
-                <Icon size={30} name={'ios-mail-open'}/>
-              </Button>
-            </Right>
+          <Header searchBar rounded>
+            <Item>
+              <Icon name="search"/>
+              <Input
+                ref={'search'}
+                placeholder='Search'
+                placeholderTextColor={'black'}
+                returnKeyType="search"
+                returnKeyLabel="search"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={(roomSearch) => this.setState({roomSearch})}
+                onSubmitEditing={() => {
+                  this.loadData(this.state.roomSearch);
+            }}/>
+            </Item>
           </Header>
           <Content
             refreshControl={
@@ -136,25 +149,6 @@ export default class Home extends Component {
             />
           </Content>
         </Container>
-        <View style={styles.searchContainer}>
-          <TextInput
-            ref={'search'}
-            style={styles.searchBar}
-            inlineLabel
-            placeholder='Search'
-            placeholderTextColor={'black'}
-            returnKeyType="search"
-            returnKeyLabel="search"
-            autoCapitalize="none"
-            autoCorrect={false}
-            onChangeText={(roomSearch) => this.setState({roomSearch})}
-            onSubmitEditing={() => {
-              this.loadData(this.state.roomSearch);
-              that.refs['search'].clear();
-            }}/>
-          <KeyboardSpacer/>
-        </View>
-      </View>
     );
   }
 
@@ -186,7 +180,7 @@ export default class Home extends Component {
 
   pressRow(rowData) {
     app.user.joinRoom(rowData._id);
-    Actions.room({room: rowData});
+    this.props.showPage('room');
   }
 }
 
